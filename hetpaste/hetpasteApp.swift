838718e92ac51abbb706bd96bdc9a44ec96d7ee0
het <hetpate384@gmail.com>
2026-06-03 16:47:21 -0400

    Initial Commit

---
+++ hetpaste/hetpasteApp.swift
@@ -0,0 +1,17 @@
+//
+//  hetpasteApp.swift
+//  hetpaste
+//
+//  Created by Het patel on 2026-06-03.
+//
+
+import SwiftUI
+
+@main
+struct hetpasteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
