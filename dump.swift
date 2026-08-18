af006eec6ba6fb95afda0f488471bf47c36e1d15
Het Patel <hpate384@gmail.com>
2026-08-17 23:50:56 -0400

    Fix AppDelegate subscription mismatch and fetch hook

--- dump.swift
+++ dump.swift
@@ -2,7 +2,13 @@ import Foundation
 import CloudKit
 
 @available(macOS 14.0, *)
-func dump() {
-    let engine = CKSyncEngine(CKSyncEngine.Configuration(database: CKContainer.default().privateCloudDatabase, stateSerialization: nil, delegate: nil!))
-    engine.state.add(pendingDatabaseChanges: [.saveZone(CKRecordZone(zoneName: "Zone"))])
+class MockDelegate: @unchecked Sendable, CKSyncEngineDelegate {
+    func handleEvent(_ event: CKSyncEngine.Event, syncEngine: CKSyncEngine) async {}
+    func nextRecordZoneChangeBatch(_ context: CKSyncEngine.SendChangesContext, syncEngine: CKSyncEngine) async -> CKSyncEngine.RecordZoneChangeBatch? { return nil }
+}
+
+@available(macOS 14.0, *)
+func dump() async throws {
+    let engine = CKSyncEngine(CKSyncEngine.Configuration(database: CKContainer.default().privateCloudDatabase, stateSerialization: nil, delegate: MockDelegate()))
+    try await engine.fetchChanges()
 }
